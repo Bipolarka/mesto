28cc8da35b6a32b4f694c2d0dd2e94484329e09b
Eldar Karakhanov <accordeur@yandex.ru>
2023-04-25 23:34:27 +0300

    hope will never die

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,39 @@
 let profileOpen = document.querySelector('.profile__changer');
 let popupClose = document.querySelector('.popup__close');
 let popupForm = document.querySelector('.popup');
-let popupButton = document.querySelector('.popup__button');
 let inputName = document.querySelector('.popup__input_type_name');
 let inputInfo = document.querySelector('.popup__input_type_info');
 
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
-let placeLikes = document.querySelectorAll('.place__like');
-
-profileOpen.addEventListener('click', function(){
-    popupForm.classList.toggle('popup_opened');
-    inputName.value = profileName.textContent;
-    inputInfo.value = profileStatus.textContent;
-});
-
-popupClose.addEventListener('click', function(){
-    popupForm.classList.toggle('popup_opened');
-});
-
-popupButton.addEventListener('click', function(event){
-    profileName.textContent = inputName.value;
-    profileStatus.textContent = inputInfo.value;
-    event.preventDefault();
-    popupForm.classList.toggle('popup_opened');
-});
-
-for (let placeLike of placeLikes) {
-    placeLike.addEventListener('click', function(){
-        placeLike.classList.toggle('place__like_active');
-    });
+function openPopup() {
+  popupForm.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputInfo.value = profileStatus.textContent;
 }
+
+function closePopup() {
+  popupForm.classList.remove('popup_opened');
+  inputName.value = '';
+  inputInfo.value = '';
+}
+
+profileOpen.addEventListener('click', function () {
+  if (popupForm.classList.contains('popup_opened')) {
+    return closePopup();
+  } else {
+    return openPopup();
+  }
+});
+
+popupClose.addEventListener('click', function () {
+  popupForm.classList.toggle('popup_opened');
+});
+
+popupForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileStatus.textContent = inputInfo.value;
+  closePopup();
+});
