069266794effba0f2b7a0351e8555276b48142f7
Eldar Karakhanov <accordeur@yandex.ru>
2023-04-26 16:14:46 +0300

    Always hope for the better

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+let profileOpen = document.querySelector('.profile__changer');
+let popupClose = document.querySelector('.popup__close');
+let popupMain = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__inputs');
+let inputName = document.querySelector('.popup__input_type_name');
+let inputInfo = document.querySelector('.popup__input_type_info');
+
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
+
+function openPopup() {
+  popupMain.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputInfo.value = profileStatus.textContent;
+}
+
+function closePopup() {
+  popupMain.classList.remove('popup_opened');
+  inputName.value = '';
+  inputInfo.value = '';
+}
+
+profileOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+popupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileStatus.textContent = inputInfo.value;
+  closePopup();
+}
+
+popupForm.addEventListener('submit', handleFormSubmit);
